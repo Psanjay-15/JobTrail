f06e9d66f27a07c44d16543aff8fb44430e050ae
Psanjay-15 <sanjaypandere26@gmail.com>
2024-07-20 19:55:48 +0530

    home responsive done

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import JobTrail from ".././assets/JobTail.png";
+import logo from ".././assets/logo.png";
+
 import "../Styles/Home.css";
 import instagram from "../assets/instagram.png";
 import facebook from "../assets/facebook.png";
@@ -12,11 +13,12 @@ function Home() {
       <div className=" home bg-gradient-to-br  from-gray-200 via-gray-100 via-40%  to-gray-200 text-white h-4/6 ">
         {/* <NavBar />
          */}
-        <div className="flex flex-row justify-between h-16 max-sm:justify-between">
-          <div className="left-box flex flex-row p-4 pt-1">
+        <div className="flex flex-row justify-between h-16 max-sm:justify-between sm:w-full">
+          <div className="left-box flex flex-row p-4 pt-1 flex items-center sm:pt-2 pl-0">
             <Link to={"/"}>
-              <img className=" w-36 h-16 max-sm:w-32 h-14" src={JobTrail} alt="" />
+              <img className=" w-38 h-12 max-sm:w-16 pt-2 pl-0 h-14" src={logo} alt="" />
             </Link>
+            <p className="font-bold text-4xl bg-gradient-to-tr from-blue-700 Montserrat to-blue-300 bg-clip-text text-transparent  hidden sm:block">JobTrail</p>
           </div>
           <div>
             <Link to={"/register"}>
@@ -35,8 +37,8 @@ function Home() {
           </div>
         </div>
         <div>
-        <div className=" home first-div  text-center py-20 m-auto p-auto flex flex-col justify-center items-center w-3/5 max-sm:pb-10 mx-auto px-auto w-5/6 ">
-          <p className=" font-bold text-8xl bg-gradient-to-tr from-indigo-700 Montserrat to-indigo-300 bg-clip-text text-transparent text-center max-sm:text-center max-sm:text-6xl 	">
+        <div className=" home first-div  text-center py-20 m-auto p-auto flex flex-col justify-center items-center w-3/5 max-sm:pb-10 mx-auto px-auto ">
+          <p className=" font-bold text-8xl bg-gradient-to-tr from-blue-700 Montserrat to-blue-300 bg-clip-text text-transparent text-center max-sm:text-center max-sm:text-5xl 	">
             Track Your Application More Efficiently
           </p>
 
@@ -59,11 +61,12 @@ function Home() {
         </div>
 
 
-        <div className="flex flex-row gap-2 mt-20 pb-10 h-4/5 max-sm:flex-col">
+        <div className="lg:flex lg:flex-row items-center justify-center gap-2 py-20 h-4/5 max-sm:flex max-sm:flex-col ">
 
-          <div className=" w-2/5 m-auto px-12 flex flex-col align-middle max-sm:w-3/5 max-sm:items-center">
-            <div className="flex max-sm:items-center">
-              <img className="w-44 h-24 pt-3 max-sm:flex w-52 h-24 align-middle items-center " src={JobTrail} />
+          <div className=" w-2/5 flex flex-col items-center">
+            <div className="flex flex-row items-center justify-center">
+              <img className="w-30 h-16" src={logo} />
+            <p className="font-bold text-4xl bg-gradient-to-tr from-blue-700 Montserrat to-blue-300 bg-clip-text text-transparent ">JobTrail</p>
             </div>
             <p className="justify-center items-center pt-4 text-gray-500 font-semibold max-sm:text-center">
               The best platform for Application tracking
@@ -83,9 +86,9 @@ function Home() {
             </div>
           </div>
 
-          <div  className="w-3/5 flex flex-row max-sm:flex max-sm:flex-row justify-around">
-          <div className=" w-2/5 px-10 mt-7 max-sm:px-5 ">
-            <p className=" text-gray-600 font-semibold text-xl pb-5 ">
+          <div  className=" w-3/5 flex flex-row justify-around max-sm:w-full max-sm:flex max-sm:flex-row max-sm:justify-around">
+          <div className=" w-auto pt-7 max-sm:text-sm max-sm:px-2 ">
+            <p className=" text-gray-600 font-semibold text-xl pb-5 sm:text-small max-sm:text-sm max-sm:px-auto">
               Product
             </p>
             <ul className="flex flex-col  text-gray-400 font-semibold leading-loose">
@@ -96,8 +99,8 @@ function Home() {
             </ul>
           </div>
 
-          <div className=" w-2/5 px-10 mt-7 max-sm:px-5 max-sm:w-1/4">
-            <p className=" text-gray-600 font-semibold text-xl pb-5 ">
+          <div className=" w-auto pt-7  max-sm:text-sm max-sm:px-2">
+            <p className=" text-gray-600 font-semibold text-xl pb-5 max-sm:text-sm  ">
               Support
             </p>
             <ul className="flex flex-col  text-gray-400 font-semibold  leading-loose">
@@ -105,24 +108,25 @@ function Home() {
                 Help center
               </li>
               <li className="hover:text-gray-500 cursor-pointer">
-                Account information
+                Account 
               </li>
               <li className="hover:text-gray-500 cursor-pointer">About</li>
               <li className="hover:text-gray-500 cursor-pointer">Contact Us</li>
             </ul>
           </div>
 
-          <div className=" w-2/5 px-10 mt-7 max-sm:px-5 max-sm:w-3/5">
-            <p className=" text-gray-600 font-semibold text-xl pb-5 ">
+          <div className=" w-auto  pt-7  max-sm:text-sm max-sm:px-2">
+            <p className=" text-gray-600 font-semibold text-xl pb-5 max-sm:text-sm">
               Help and Solutions
             </p>
             <ul className="flex flex-col  text-gray-400 font-semibold leading-loose">
               <li className="hover:text-gray-500 cursor-pointer">
-                Talk to support
+                Support 
               </li>
               <li className="hover:text-gray-500 cursor-pointer">
-                Support Docs
+                Documents
               </li>
+
               <li className="hover:text-gray-500 cursor-pointer">
                 System status
               </li>
